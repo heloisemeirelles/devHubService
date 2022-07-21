20ec3cd2aa7f9b2d69e296f67c85796fe515ffed
Heloise Vadt Meirelles <CIANDT\heloisem@lnb035710cps.ciandt.global>
2022-07-21 15:27:24 -0300

    first commit

--- serviceLibrary/src/main/java/android/raywenderlich/servicelibrary/favoriteUserDatabase/FavoriteDao.kt
+++
@@ -1,5 +0,0 @@
-package android.raywenderlich.servicelibrary.favoriteUserDatabase
-
-interface FavoriteDao {
-
-}
--- serviceLibrary/src/main/java/android/raywenderlich/servicelibrary/favoriteUserDatabase/FavoriteDatabase.kt
+++
@@ -1,4 +0,0 @@
-package android.raywenderlich.servicelibrary.favoriteUserDatabase
-
-class FavoriteDatabase {
-}
